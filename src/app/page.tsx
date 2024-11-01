b5d72a9bbbe713f54962816b566b6fe4afa81abb
ZubaydullaCoder <zubaydullacoder@gmail.com>
2024-11-01 07:59:42 +0500

    browser tab logo changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import DefaultLayout from '@/components/Layouts/DefaultLayout';
 import React from 'react';
 
 export const metadata: Metadata = {
-  title: 'Next.js E-commerce Dashboard Page | NextAdmin - Next.js Dashboard Kit',
+  title: 'Next.js E-commerce Dashboard Page |  Next.js dashboard by Zubaydulla',
   description: 'This is Next.js Home page for NextAdmin Dashboard Kit',
 };
 
